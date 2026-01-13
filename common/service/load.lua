cb243eb8fce68fe0fae433fbca1c82df3a1365aa
monsoon <97245589@qq.com>
2026-01-12 22:45:09 -0700

    load

--- common/service/load.lua
+++ common/service/load.lua
@@ -1,19 +1,25 @@
-local SERVICE_NAME = SERVICE_NAME
 local string = string
 local io = io
+local require = require
 
 local load_files = function(depth)
+    local SERVICE_NAME = SERVICE_NAME
     local service_dir = SERVICE_NAME:gsub("/[^/]+$", "")
     local str = string.format('find %s -maxdepth %s -mindepth %s -name "*.lua"', service_dir, depth, depth)
     local f = io.popen(str)
     for line in f:lines() do
-        
+        local file_name = string.sub(line, 1, -5)
+        if file_name ~= SERVICE_NAME then
+            local m = string.gsub(file_name, '/', '.')
+            require(m)
+        end
     end
     f:close()
 end
 
 local load = function()
     load_files(1)
+    load_files(2)
 end
 
 return {
