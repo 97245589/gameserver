2b262d18c222c1986e6972e07cd7364d50c67554
monsoon <97245589@qq.com>
2026-02-06 18:24:21 -0700

    game init

--- server/game/init.lua
+++ server/game/init.lua
@@ -5,16 +5,14 @@ local skynet = require "skynet"
 require "common.func.tool"
 
 skynet.start(function()
-    local startarr = {
-        "player", 3, "watchdog", 1, "cluster", 1
+    local service_num = {
+        player = 3,
+        watchdog = 1,
+        cluster = 1
     }
-    local service_num = {}
     local addrs = {}
 
-    for i = 1, #startarr, 2 do
-        local name = startarr[i]
-        local num = startarr[i + 1]
-        service_num[name] = num
+    for name, num in pairs(service_num) do
         local init = "server/game/" .. name .. "/init"
         if num == 1 then
             addrs[name] = skynet.newservice(init)
