69666c608afb58b8e5d666fcdce0d467ce8fc9d9
monsoon <97245589@qq.com>
2026-09-18 16:02:18 -0600

    crc16

--- clib/game/ltool.h
+++ clib/game/ltool.h
@@ -6,7 +6,7 @@
 #include <string_view>
 
 struct Ltool {
-  static uint16_t crc16(const char* p, int len);
+  static uint16_t crc16(const unsigned char *buf, size_t len);
   static bool zstd_compress(std::string_view str, std::string& ret, int level);
   static bool zstd_decompress(std::string_view str, std::string& ret);
 };
